2ede948207cb2b49efcbfd44464338a44f9fe437
JoseRosas283 <josealfredorosascanuto@gmail.com>
2025-09-27 15:59:42 -0600

    App practica

--- app/(tabs)/carrito.tsx
+++ app/(tabs)/carrito.tsx
@@ -21,6 +21,20 @@ export default function Carrito() {
             descripcion: "Descripción detallada del primer producto",
             precio: 29.99,
             imagen: require("@/assets/images/banner.png")
+        },
+        {
+            id: 2,
+            nombre: "Producto 2",
+            descripcion: "Descripción detallada del segundo producto",
+            precio: 45.50,
+            imagen: require("@/assets/images/banner.png")
+        },
+        {
+            id: 3,
+            nombre: "Producto 3",
+            descripcion: "Descripción detallada del tercer producto",
+            precio: 15.99,
+            imagen: require("@/assets/images/banner.png")
         }
-    ])
+    ]);
 }
